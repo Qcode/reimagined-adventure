b365d2cea05a323ce9e3c877e425bff60c55a93e
Ross Evans <therealrossevans@gmail.com>
2017-07-08 23:28:01 -0300

    Add Player

---
+++ classes/gameObjects/Player.lua
@@ -0,0 +1,17 @@
+Player = class:new()
+Player:addparent(GameObject)
+
+function Player:new(properties)
+end
+
+function Player:update(dt)
+end
+
+function Player:draw()
+end
+
+function Player:keypressed(key)
+end
+
+function Player:mousepressed(x, y, button)
+end
--- loadFiles.lua
+++ loadFiles.lua
@@ -1,8 +1,11 @@
 require 'lib/class'
 require 'classes/GameManager'
 require 'classes/GameState'
+require 'classes/GameObject'
 
 require 'classes/gameStates/Menu'
 require 'classes/gameStates/Game'
 
 require 'classes/utils/Timer'
+
+require 'classes/gameObjects/Player'
